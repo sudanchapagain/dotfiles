ac9abfe8b07d37d9772e12beb3672f1750042898
Your Name <you@example.com>
2023-12-31 18:01:04 +0545

    Fanciest it's ever been

--- init.lua
+++ init.lua
@@ -11,154 +11,152 @@ vim.g.maplocalleader = ' '
 --                   EDITOR
 ------------------------------------------------
 
--- line numbers in the gutter.
+-- Line numbers in the gutter.
 vim.opt.number = true
--- relative line numbers.
-vim.opt.relativenumber = false
--- line wrapping.
+-- Relative line numbers.
+vim.opt.relativenumber = true
+-- Color Column
+vim.opt.colorcolumn = "70"
+-- Line wrapping.
 vim.opt.wrap = true
 vim.opt.linebreak = true
--- the number of spaces a tab character counts for.
+-- The number of spaces a tab character counts for.
 vim.opt.tabstop = 2
--- the number of spaces for a "soft" tabstop.
+-- The number of spaces for a "soft" tabstop.
 vim.opt.softtabstop = 2
--- the number of spaces used for each level of (auto)indentation.
+-- The number of spaces used for each level of (auto)indentation.
 vim.opt.shiftwidth = 2
--- expanding tabs into spaces. Tab key inserts spaces instead of a tab character.
+-- Expanding tabs into spaces. Tab key inserts spaces instead of a tab character.
 vim.opt.expandtab = true
--- rounding the indentation to a multiple of 'shiftwidth'.
+-- Rounding the indentation to a multiple of 'shiftwidth'.
 vim.opt.shiftround = true
--- smart auto-indenting.
+-- Smart auto-indenting.
 vim.opt.smartindent = true
--- incremental searching. As you type a search pattern highlights the matching text in real-time.
+-- Incremental searching. As you type a search pattern highlights the matching text in real-time.
 vim.opt.incsearch = true
--- sets the default file format to Windows (CRLF) line endings.
+-- Sets the default file format to Windows (CRLF) line endings.
 vim.opt.fileformat = 'dos'
--- true color support for the terminal.
+-- True color support for the terminal.
 vim.opt.termguicolors = true
--- mouse support in all modes, 'a' for 'all'.
+-- Mouse support in all modes, 'a' for 'all'.
 vim.opt.mouse = 'a'
--- Highlight search results as you type for specific file types
+-- Highlight search results as you type for specific file types.
 vim.cmd([[autocmd FileType markdown,text setlocal spell]])
--- Display line endings in the status line using the Airline plugin
-vim.g.airline_section_z = '%{&fileformat}'
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+-- See `:help 'clipboard'`.
 vim.o.clipboard = 'unnamedplus'
+-- Theme
+vim.cmd.colorscheme 'habamax'
 
 ------------------------------------------------
---                   EDITOR
+--        LAZY PACKAGE MANAGER
 ------------------------------------------------
 
 -- Check if lazy is installed if not git clone it.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
--- adds lazy into neovim environment runtime.
+
+-- Adds lazy into neovim environment runtime.
 vim.opt.rtp:prepend(lazypath)
 
+-- PACKAGES
 require("lazy").setup({
-  "folke/which-key.nvim",
-  "nvim-lua/plenary.nvim",
-  -- theme
-  {
-    "sainnhe/sonokai",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'sonokai'
-    end,
-  },
-  -- Git integration
-  "tpope/vim-fugitive",
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
+    -- Simple Packages
+    "nvim-lua/plenary.nvim",
+    "ThePrimeagen/vim-be-good",
+    -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            {
+                'williamboman/mason.nvim',
+                config = function()
+                    require("mason").setup {}
+                end,
+            },
+            'williamboman/mason-lspconfig.nvim',
+            { 'j-hui/fidget.nvim', opts = {} },
+        },
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        end
     },
-  },
 
-  {
     -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            -- Adds LSP completion capabilities
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+            -- Adds a number of user-friendly snippets
+            'rafamadriz/friendly-snippets',
+        },
     },
-  },
 
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        },
+        lazy = false,
+    },
     -- Fuzzy Finder (files, lsp, etc)
     {
-      'nvim-telescope/telescope.nvim',
-      branch = '0.1.x',
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-        -- Only load if `make` is available. Make sure you have the system
-        -- requirements installed.
-        {
-          'nvim-telescope/telescope-fzf-native.nvim',
-          -- NOTE: If you are having trouble with this installation,
-          --       refer to the README for telescope-fzf-native for more instructions.
-          build = 'make',
-          cond = function()
-            return vim.fn.executable 'make' == 1
-          end,
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make',
+                cond = function()
+                    return vim.fn.executable 'make' == 1
+                end,
+                config = function()
+                    local builtin = require('telescope.builtin')
+                    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+                    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+                    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+                    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+                end,
+            },
         },
-      },
+    },
+    {
+        -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ':TSUpdate',
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                highlight = {
+                    enable = true,
+                }
+            }
+        end
+
     },
 
-    {
-      -- Highlight, edit, and navigate code
-      'nvim-treesitter/nvim-treesitter',
-      dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-      },
-      build = ':TSUpdate',
-    },
-  
 })
 
--- Treesitter setup
 
-require("nvim-treesitter.configs").setup {
-  highlight = {
-    enable = true,
-  },
-  require = {'nvim-lua/plenary.nvim'}, 
-}
+
+require('Comment').setup()
 
 -- Mason setup
-require("mason").setup{}
-
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
