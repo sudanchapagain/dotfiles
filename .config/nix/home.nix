c3b118752e6f82c52908ebd89e06705247888bfe
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-03-25 20:45:30 +0545

    basic changes + doesn't build rn

--- .config/nix/home.nix
+++ .config/nix/home.nix
@@ -2,88 +2,58 @@
 
 {
   home.username = "crimson";
+  home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
   home.homeDirectory = "/home/crimson";
 
   # link the configuration file in current directory to the specified location in home directory
   # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
   # link all files in `./scripts` to `~/.config/i3/scripts`
   # home.file.".config/i3/scripts" = {
   #   source = ./scripts;
   #   recursive = true;   # link recursively
   #   executable = true;  # make all files executable
   # };
-
   # encode the file content in nix configuration file directly
   # home.file.".xxx".text = ''
   #     xxx
   # '';
 
-  # set cursor size and dpi for 4k monitor
   xresources.properties = {
     "Xcursor.size" = 16;
     "Xft.dpi" = 172;
   };
 
-  # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
-
     neofetch
-    nnn # terminal file manager
+    yazi
 
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    eza # A modern replacement for ‘ls’
-    fzf # A command-line fuzzy finder
+    ripgrep
+    jq
+    eza
+    fzf
 
-    # misc
-    cowsay
-    file
     which
     tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    gnupg
 
     # nix related
     # it provides the command `nom` works just like `nix`
     # with more details log output
     nix-output-monitor
 
-    # productivity
-    glow # markdown previewer in terminal
-    btop # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
+    glow 
+    btop
 
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
-
-    # system tools
-    sysstat
-    lm_sensors # for `sensors` command
-    ethtool
-    pciutils # lspci
-    usbutils # lsusb
   ];
 
-  # basic configuration of git, please change to your own
-  #  programs.git = {
-  #    enable = true;
-  #    userName = "";
-  #    userEmail = "xiaoyin_c@qq.com";
-  #  };
+   programs.git = {
+     enable = true;
+     userName = "Sudan Chapagain";
+     userEmail = "101015050+sudanchapagain@users.noreply.github.com";
+   };
 
-  # starship - an customizable prompt for any shell
-  # programs.starship = {
-  #  enable = true;
+  programs.starship = {
+    enable = true;
   # # custom settings
   #settings = {
   # add_newline = false;
@@ -91,20 +61,47 @@
   #      gcloud.disabled = true;
   #     line_break.disabled = true;
   #  };
-  #  };
+    };
 
   # alacritty - a cross-platform, GPU-accelerated terminal emulator
   programs.alacritty = {
     enable = true;
-    # custom settings
     settings = {
       env.TERM = "xterm-256color";
+
+      window = {
+        padding.x = ;
+        padding.y = ;
+        decoration = "";
+        startup_mode = "";
+        dynamic_title = true;
+        opacity = ;
+      };
+
+      cursor = {
+        style = {
+          shape = "Bean";
+          blinking = "On"
+          };
+      };
+      live_config_reload = true;
+
       font = {
-        size = 12;
+        size = 14;
+        normal.family = "JetBrainsMono Nerd Font";
+        bold.family = "JetBrainsMono Nerd Font";
+        italic.family = "JetBrainsMono Nerd Font";
+        bold_italic.family = "JetBrainsMono Nerd Font";
+      };
+
+      scrolling = {
+        multiplier = 5;
+        history = 10000;
+      };
+
+      colors = {
         draw_bold_text_with_bright_colors = true;
       };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
     };
   };
 
@@ -116,24 +113,9 @@
     #    export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     # '';
 
-    # set some aliases, feel free to add more or remove some
-    #shellAliases = {
+    shellAliases = {
     # k = "kubectl";
-    #urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-    #urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-    # };
+     };
   };
 
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.11";
-
-  # Let home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
