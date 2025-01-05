78d5edf57df74fbf05508557ba6639cc9d8d1825
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-01-05 20:50:25 +0545

    .

--- home/fish/functions/fish_greeting.fish
+++ home/fish/functions/fish_greeting.fish
@@ -1,2 +1,5 @@
 function fish_greeting
+    fastfetch -c examples/8.jsonc
+    set -x GOPATH $HOME/go
+    set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 end
--- system/scripts/ghostty.sh
+++ system/scripts/ghostty.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# doas apk add distrobox
+# distrobox-create --name zig-container --image alpine:latest
+# distrobox-enter zig-container
+# doas vi /etc/apk/repositories
+#   Append the following lines
+#   https://dl-cdn.alpinelinux.org/alpine/edge/main
+#   https://dl-cdn.alpinelinux.org/alpine/edge/community
+# doas apk update
+# doas apk upgrade
+# doas apk add zig libadwaita-dev gtk4.0-dev
+
+git clone https://github.com/ghostty-org/ghostty .
+
+zig build -Doptimize=ReleaseFast
+
 #mkdir -p ~/.local/bin
 #mkdir -p ~/.local/share/applications
 #mkdir -p ~/.local/share/icons/hicolor/16x16/apps
@@ -10,6 +25,7 @@
 #mkdir -p ~/.local/share/icons/hicolor/512x512/apps
 
 cp zig-out/bin/ghostty ~/.local/bin/
+
 #cp zig-out/share/applications/com.mitchellh.ghostty.desktop ~/.local/share/applications/
 
 #cp "images/icons/icon_16x16.png" ~/.local/share/icons/hicolor/16x16/apps/ghostty.png
