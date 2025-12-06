f6e46ceac70be0e64ca8c80ab6bef4a769fc7e09
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-12-06 08:12:23 +0545

    mpd
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- system/services.nix
+++ system/services.nix
@@ -30,5 +30,40 @@
         logind.settings.Login = {
             HandlePowerKey = "suspend";
         };
+
+        mpd = {
+            enable = true;
+            startWhenNeeded = true;
+            network.listenAddress = "any";
+
+            extraConfig = ''
+                music_directory "~/Music"
+                playlist_directory "~/.config/mpd/playlists"
+
+                db_file "~/.config/mpd/database"
+                log_file "~/.config/mpd/log"
+                pid_file "~/.config/mpd/pid"
+                state_file "~/.config/mpd/state"
+                sticker_file "~/.config/mpd/sticker.sql"
+
+                bind_to_address "127.0.0.1"
+                port "6600"
+
+                metadata_to_use "artist,album,title,track,name,genre,date,composer,performer,disc"
+
+                input {
+                    plugin "curl"
+                }
+
+                audio_output {
+                    type "pulse"
+                    name "Pulse Output"
+                }
+
+                save_absolute_paths_in_playlists "no"
+                auto_update "yes"
+                auto_update_depth "3"
+            '';
+        };
     };
 }
